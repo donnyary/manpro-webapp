41755386f31d04025fe16280f2af43f43135ef1a
Donny Aryanto <donnyary@yahoo.com>
2026-08-30 15:50:27 +0700

    Auto: daily.py

--- daily.py
+++ daily.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, g
 import mysql.connector
 import os
 import re
